86c1f4846fa2ed9af572e728604e1328630e4abc
Phil <souzaherodp@gmail.com>
2022-03-12 15:59:26 -0500

    create custom heat map

--- src/components/heatMap/heatMap.js
+++ src/components/heatMap/heatMap.js
@@ -1,51 +1,118 @@
 import React from 'react';
-//import CalendarHeatmap from '@freecodecamp/react-calendar-heatmap';
-import '@freecodecamp/react-calendar-heatmap/dist/styles.css';
-import { CalendarHeatmap } from 'reaviz';
-import { useEffect,useState } from 'react';
-
-
+import './heatMap.css';
+import {useEffect,useState} from 'react';
 
 function HeatMap(props) {
 
-    const [monthData, setMonthData] = useState([
-        {
-            "key": new Date("03-01-2021"),
-            "data": 3
-          }
-    ]);
+    const widthStyle = {
+        width:'100%'
+    }
+
+    const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+    ]
+
+
+    const [data, setData] = useState(props.data)
 
     useEffect(() => {
-        if (!props.data) return;
 
-        let newMonthData = []
+        console.log("Updating data for heat map")
 
-        for (let i = 0; i < props.data.length; i++) {
-            newMonthData.push(
-                {
-                    "key": props.data[i].time,
-                    "data": props.data[i].value
-                }
-            )
+        let newData = []
+
+        //find max value
+        let max = 0;
+
+        for(let i = 0; i < props.data.length; i++) {
+            if (props.data[i].value > max) {
+                
+                max = props.data[i].value
+            }
         }
 
-        setMonthData(newMonthData)
-        
+        if (max === 0 ) return;
+
+        // calculate color value
+
+        for (let i = 0; i < props.data.length; i++ ) {
+            if( props.data[i].value/max <0.25) {
+                newData.push({
+                    time: props.data[i].time,
+                    value: props.data[i].value,
+                    color: 1
+                })
+            } else if (props.data[i].value/max < 0.5) {
+                newData.push({
+                    time: props.data[i].time,
+                    value: props.data[i].value,
+                    color: 2
+                })
+            } else if (props.data[i].value/max <0.75) {
+                newData.push({
+                    time: props.data[i].time,
+                    value: props.data[i].value,
+                    color: 3
+                })
+            } else {
+                newData.push({
+                    time: props.data[i].time,
+                    value: props.data[i].value,
+                    color: 4
+                })
+            }
+        }
+
+
+        setData(newData);
+
+
     },[props.data])
 
 
     return (
         <React.Fragment>
-           <CalendarHeatmap
-                height={350}
-                width={350}
-                view={props.view}
-                data={monthData}
-                margins={20}
-            />
+            <div style={widthStyle} className={"yearContainer " + widthStyle}>
+                <div className="monthContainer">
+                    {
+                        months.map((obj, monthKey) => {
+                            return (
+                                <React.Fragment>
+                                    <div className="month">
+                                        {obj}
+                                    </div>
+                                    <div className="dayContainer">
+                                    {data && data.map( (obj, key) => {
+                                        if(obj.time.getMonth() !== monthKey)  return (null)
+
+                                        return (
+                                            <div key={key} className={"day " + "color-scale-" + obj.color}>
+                                            </div>
+                                        )
+                                    })}
+                                    
+                                    </div>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                
+                </div>
+            </div>
+
         </React.Fragment>
     )
-
 }
 
 
