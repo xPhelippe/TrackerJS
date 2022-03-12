46c8791f44518e54cd0fa76a507743076ae59aa8
Phil <souzaherodp@gmail.com>
2022-03-12 15:58:41 -0500

    create week view

---
+++ src/components/weeklyChartView/weeklyChartView.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { XAxis, YAxis, BarChart, Bar} from 'recharts';
+import './weeklyChartView.css'
+
+function WeeklyChartView(props) {
+
+    const data = props.data
+
+    return (
+        <React.Fragment>
+        <div className="barChart">
+            <BarChart width={800} height={400} data={data} className="barChart" >
+                <Bar type="monotone" dataKey="value" fill="#285238"/>
+                <XAxis 
+                    dataKey="time"
+                    type="category"
+                    className="barChart"
+                    stroke="#285238"/>
+                <YAxis 
+                    stroke="#285238"/>
+                
+            </BarChart>
+        </div>
+        </React.Fragment>
+    )
+
+}
+
+export default WeeklyChartView;
