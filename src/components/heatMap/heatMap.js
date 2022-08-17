cc7655778961e5bcd02fb20163350bd448ed9a33
Phil <souzaherodp@gmail.com>
2022-08-17 10:27:08 -0400

    make yearly view responsive

--- src/components/heatMap/heatMap.js
+++ src/components/heatMap/heatMap.js
@@ -1,119 +1,109 @@
-import React from 'react';
-import './heatMap.css';
-import {useEffect,useState} from 'react';
-
+import React from "react";
+import "./heatMap.scss";
+import { useEffect, useState } from "react";
+import { Container } from "@mui/material";
 function HeatMap(props) {
-
-    const widthStyle = {
-        width:'100%'
-    }
-
     const months = [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec'
-    ]
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
 
-
-    const [data, setData] = useState(props.data)
+    const [data, setData] = useState(props.data);
 
     useEffect(() => {
+        console.log("Updating data for heat map");
 
-        console.log("Updating data for heat map")
-
-        let newData = []
+        let newData = [];
 
         //find max value
         let max = 0;
 
-        for(let i = 0; i < props.data.length; i++) {
+        for (let i = 0; i < props.data.length; i++) {
             if (props.data[i].value > max) {
-                
-                max = props.data[i].value
+                max = props.data[i].value;
             }
         }
 
-        if (max === 0 ) return;
+        if (max === 0) return;
 
         // calculate color value
 
-        for (let i = 0; i < props.data.length; i++ ) {
-            if( props.data[i].value/max <0.25) {
+        for (let i = 0; i < props.data.length; i++) {
+            if (props.data[i].value / max < 0.25) {
                 newData.push({
                     time: props.data[i].time,
                     value: props.data[i].value,
-                    color: 1
-                })
-            } else if (props.data[i].value/max < 0.5) {
+                    color: 1,
+                });
+            } else if (props.data[i].value / max < 0.5) {
                 newData.push({
                     time: props.data[i].time,
                     value: props.data[i].value,
-                    color: 2
-                })
-            } else if (props.data[i].value/max <0.75) {
+                    color: 2,
+                });
+            } else if (props.data[i].value / max < 0.75) {
                 newData.push({
                     time: props.data[i].time,
                     value: props.data[i].value,
-                    color: 3
-                })
+                    color: 3,
+                });
             } else {
                 newData.push({
                     time: props.data[i].time,
                     value: props.data[i].value,
-                    color: 4
-                })
+                    color: 4,
+                });
             }
         }
 
-
         setData(newData);
-
-
-    },[props.data])
+    }, [props.data]);
 
     return (
         <React.Fragment>
             <h2>{props.data[0].time.getFullYear()}</h2>
-            <div style={widthStyle} className="yearContainer">
+            <div className="yearContainer">
                 <div className="monthContainer">
-                    {
-                        months.map((obj, monthKey) => {
-                            return (
-                                <React.Fragment>
-                                    <div className="month">
-                                        {obj}
-                                    </div>
-                                    <div className="dayContainer">
-                                    {data && data.map( (obj, key) => {
-                                        if(obj.time.getMonth() !== monthKey)  return (null)
+                    {months.map((obj, monthKey) => {
+                        return (
+                            <React.Fragment>
+                                <div className="month">{obj}</div>
+                                <div className="dayContainer">
+                                    {data &&
+                                        data.map((obj, key) => {
+                                            if (
+                                                obj.time.getMonth() !== monthKey
+                                            )
+                                                return null;
 
-                                        return (
-                                            <div key={key} className={"day color-scale-" + obj.color}>
-                                            </div>
-                                        )
-                                    })}
-                                    
-                                    </div>
-                                </React.Fragment>
-                            )
-                        })
-                    }
-                
+                                            return (
+                                                <div
+                                                    key={key}
+                                                    className={
+                                                        "day color-scale-" +
+                                                        obj.color
+                                                    }
+                                                ></div>
+                                            );
+                                        })}
+                                </div>
+                            </React.Fragment>
+                        );
+                    })}
                 </div>
             </div>
-
         </React.Fragment>
-    )
+    );
 }
-
 
 export default HeatMap;
