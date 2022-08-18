4f7789413efac01bbc833609f6188c93dee1373c
Phil <souzaherodp@gmail.com>
2022-08-18 12:10:11 -0400

    make navbar responsive

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import './navbar.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import Button from "@mui/material/Button";
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar(props) {
-
-    const links = [
-        'Home',
-        'About',
-        'Demo',
-        'Edit'
-    ]
+    const links = ["Home", "About", "Demo", "Edit"];
 
     return (
         <React.Fragment>
             <div className="navbarContainer">
-                {links.map((obj,key) => {
+                {links.map((obj, key) => {
                     return (
-                        <Link 
-                        key={key}
-                        to={"/" + obj.toLowerCase()}
-                        style={{textDecoration:'inherit', color:'inherit'}}
-                        ><Button variant="Text">{obj}</Button>
+                        <Link
+                            key={key}
+                            to={"/" + obj.toLowerCase()}
+                            style={{
+                                textDecoration: "inherit",
+                                color: "inherit",
+                            }}
+                        >
+                            <Button variant="Text">{obj}</Button>
                         </Link>
-
-                    )
-                    
+                    );
                 })}
 
-                <a 
-                    href="https://github.com/xPhelippe/TrackerJS" 
-                    style={{textDecoration:'inherit', color:'inherit'}}
-                    target="_blank" rel="noopener noreferrer"
-                    ><Button variant="Text">Github</Button></a>
+                <a
+                    href="https://github.com/xPhelippe/TrackerJS"
+                    style={{ textDecoration: "inherit", color: "inherit" }}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Button variant="Text">Code</Button>
+                </a>
             </div>
         </React.Fragment>
-    )
-
+    );
 }
-
 
 export default Navbar;
