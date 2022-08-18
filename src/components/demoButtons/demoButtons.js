5d2d5e85d1b3ad406bd9dd90cabd2e7f146fa5d3
Phil <souzaherodp@gmail.com>
2022-08-18 12:11:46 -0400

    prettify code for demo buttons

--- src/components/demoButtons/demoButtons.js
+++ src/components/demoButtons/demoButtons.js
@@ -1,54 +1,45 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import './demoButtons.css'
-import DataGenerator from '../../utils/createData';
+import React from "react";
+import Button from "@mui/material/Button";
+import "./demoButtons.css";
+import DataGenerator from "../../utils/createData";
 
 function DemoButtons(props) {
-
-
     function randomClicked(evt) {
-        let newData = DataGenerator.random()
+        let newData = DataGenerator.random();
 
         props.setData(newData);
-
     }
 
     function zigZagClicked(evt) {
-        let newData= DataGenerator.zigZag()
+        let newData = DataGenerator.zigZag();
 
-        props.setData(newData)
+        props.setData(newData);
     }
 
     function strongClicked(evt) {
-        let newData = DataGenerator.strongSoFar()
+        let newData = DataGenerator.strongSoFar();
 
-        props.setData(newData)
+        props.setData(newData);
     }
 
     return (
         <React.Fragment>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: "center", marginTop: "1rem" }}>
                 <p>Click a button to see what the charts look like when full</p>
                 <div className="divButContainer">
-                    
-                        
-                    <Button variant="contained"
-                        onClick={randomClicked}>Random</Button>
-                    <Button variant="contained"
-                        onClick={zigZagClicked}>Zig Zag</Button>
-                    <Button variant="contained"
-                        onClick={strongClicked}>Strong so far</Button>
-
-                        
-                        
-                    
+                    <Button variant="contained" onClick={randomClicked}>
+                        Random
+                    </Button>
+                    <Button variant="contained" onClick={zigZagClicked}>
+                        Zig Zag
+                    </Button>
+                    <Button variant="contained" onClick={strongClicked}>
+                        Strong so far
+                    </Button>
                 </div>
             </div>
-            
         </React.Fragment>
-    )
-
+    );
 }
-
 
 export default DemoButtons;
